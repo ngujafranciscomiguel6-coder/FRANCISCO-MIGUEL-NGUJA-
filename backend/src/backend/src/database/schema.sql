c1ee857d8457aeb46dd67b052c1db2f1328e166e
ngujafranciscomiguel6-coder <ngujafranciscomiguel6@gmail.com>
2026-07-29 09:20:21 +0200

    Create users table in schema.sql

---
+++ backend/src/backend/src/database/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    senha VARCHAR(255),
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
